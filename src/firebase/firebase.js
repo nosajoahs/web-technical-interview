dea93ac3b3f38462a0b4d7e46de83436d365d097
Jason Shao <jason.shao.dev@gmail.com>
2023-12-18 14:58:56 -0800

    Add base files and import data to component

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCThUlGWTEir7dMkaHE70KMxyzr9UOi6H0",
+  authDomain: "revive-wti.firebaseapp.com",
+  projectId: "revive-wti",
+  storageBucket: "revive-wti.appspot.com",
+  messagingSenderId: "206991576055",
+  appId: "1:206991576055:web:5cdf9e6ab655fda4c2fcf2",
+  measurementId: "G-2Q7PC28C2S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firestoreDB = getFirestore(app)
---
+++ src/pages/home.tsx
@@ -0,0 +1,50 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+import Image from "next/image"
+import styles from "../styles/Home.module.css"
+import homes from "../../data/homes.json";
+import reviveLogo from '../../public/images/reviveLogo.png'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Web Technical Interview</title>
+      </Head>
+      <main className={styles.main}>
+        <span className={styles.logo}>
+          <Image src={reviveLogo} alt="Revive Logo" />
+        </span>
+
+        <ul>
+        {homes.map((home, id) => (
+          <li key= {id} className="my-4 w-full flex justify-between">
+            <div className="p-4 w-full flex justify-between">
+              <span className="capitalize">{home.address}</span>
+              <span className="capitalize">{home.fullAddress}</span>
+              <span>{home.city}</span>
+              <span>{home.state}</span>
+              <span>{home.zipCode}</span>
+              <span>{home.city}</span>
+              <span>{home.state}</span>
+              <span>{home.zipCode}</span>
+            </div>
+          </li>
+        ))}
+        </ul>
+
+        <div className={styles.grid} />
+
+        <div className={styles.card} />
+      </main>
+
+      <footer className={styles.footer}>
+        <span className={styles.logo}>
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </span>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
